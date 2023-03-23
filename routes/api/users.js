f264f8f581ee4a81d5693e78a55579106f1a14b1
Ivan Dovhopol <ivandovhopol@gmail.com>
2023-03-24 01:40:43 +0200

    обновляет пути

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { auth, validation, ctrlWrapper, upload } = require('../../middlewares');
-const { joiSchema, joiLoginSchema, emailSchema } = require('../../models/user');
+const { joiSchema, joiLoginSchema, emailSchema, joiSubscriptionSchema } = require('../../models/user');
 const { users: ctrl } = require('../../controllers');
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login));
 router.get('/logout', auth, ctrlWrapper(ctrl.logout));
 
 router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar));
+router.patch('/:id/subscription', auth, validation(joiSubscriptionSchema), ctrlWrapper(ctrl.updSubscription));
 router.get('/verify/:verificationToken', ctrlWrapper(ctrl.verifyEmail));
 router.post('/verify', validation(emailSchema), ctrlWrapper(ctrl.resendVerifyEmail));
 
