c1827b05bf1186225c7ad4a556f33160fb4628cc
Ivan <ivandovhopol@gmail.com>
2024-03-23 03:15:09 +0200

    fix connect to db

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
 const app = require('../app');
 require('colors');
 
-const { DB_HOST, PORT = 3001 } = process.env;
+const { PORT = 3001 } = process.env; // DB_HOST
 mongoose.set('strictQuery', false);
 
 mongoose
-  .connect(DB_HOST)
+  .connect(/* DB_HOST */ uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(app.listen(PORT, console.log('Database connection successful:', `http://localhost:${PORT}/api/`.blue)))
   .catch(error => {
     console.log(error.message);
