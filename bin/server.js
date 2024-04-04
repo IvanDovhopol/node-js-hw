d44108d2be15141e62ad8face204dd1977ad5437
Ivan <ivandovhopol@gmail.com>
2024-04-04 19:19:56 +0300

    fix server

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI;
 const app = require('../app');
 require('colors');
 
-const { PORT = 3001 } = process.env; // DB_HOST
+const { PORT = 3001, MONGODB_URI } = process.env;
 mongoose.set('strictQuery', false);
 
 mongoose
-  .connect(/* DB_HOST */ uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(app.listen(PORT, console.log('Database connection successful:', `http://localhost:${PORT}/api/`.blue)))
+  .connect(MONGODB_URI)
+  .then(app.listen(PORT, console.log('Database connection successful')))
   .catch(error => {
     console.log(error.message);
     process.exit(1);
